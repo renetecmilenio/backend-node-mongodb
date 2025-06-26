187b52068fd4aebf480d9583ff9eec1f91d58d80
reneceron <rene.ceron@tecmilenio.mx>
2025-06-25 20:27:59 -0600

    ruta protegida JWT

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 
 const authRoutes = require('./routes/auth.routes')
 const serviceRoutes = require('./routes/service.routes')
+const userRoutes = require('./routes/user.routes')
 
 // Middleware
 app.use(express.json())
@@ -13,6 +14,7 @@ app.use(helmet())
 // Routes
 app.use('/api/auth', authRoutes)
 app.use('/api/services', serviceRoutes)
+app.use('/api/users', userRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
