e916e60a941924d81dc88223ea6050543134091f
René <reno7882@gmail.com>
2025-07-14 19:28:09 -0600

    corrigiendo merge de git

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose')
 const { MongoMemoryServer } = require('mongodb-memory-server')
 
@@ -36,41 +35,3 @@ module.exports = {
     }
   }
 }
-=======
-const mongoose = require('mongoose')
-const { MongoMemoryServer } = require('mongodb-memory-server')
-
-let mongoServer
-
-module.exports = {
-  connect: async () => {
-    mongoServer = await MongoMemoryServer.create()
-    const uri = mongoServer.getUri()
-    await mongoose.connect(uri, {
-      dbName: 'test'
-    })
-  },
-
-  closeDatabase: async () => {
-    try {
-      if (mongoose.connection.readyState === 1) {
-        await mongoose.connection.dropDatabase()
-        await mongoose.connection.close()
-      }
-      if (mongoServer) {
-        await mongoServer.stop()
-      }
-    } catch (error) {
-      console.error('Error closing database:', error)
-      throw error
-    }
-  },
-
-  clearDatabase: async () => {
-    const collections = mongoose.connection.collections
-    for (const key in collections) {
-      await collections[key].deleteMany({})
-    }
-  }
-}
->>>>>>> d4e75b32c5873392989a5155d7adb24f8f81de50
