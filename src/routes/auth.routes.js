35844bfa4a8cb0c47ed76904c14896a8798bb6bc
René <reno7882@gmail.com>
2025-07-16 11:52:16 -0600

    creamos la rama docker

--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { register, login } = require('../controllers/auth.controller')
-const { validateRegister, validateLogin } = require('../middlewares/validators/auth.validator')
-
-router.post('/register', validateRegister, register)
-router.post('/login', validateLogin, login)
-
-module.exports = router
